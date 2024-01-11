757b9b73ddace8781799d9573d2f4735e1e7b497
GoHyunkyu <gusrb0296@naver.com>
2024-01-11 20:07:12 +0900

    [💡Feature] Player LIfeCycle
    플레이어 라이프 사이클 (이동 - 공격)을 만들었습니다.

--- IdleGame/Assets/Scripts/Main/Model/Player.cs
+++ IdleGame/Assets/Scripts/Main/Model/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,8 +11,19 @@ public class Player : MonoBehaviour
     public float CriticalPercent { get; private set; }
     public float CriticalDamage { get; private set; }
     public float Range { get; private set; }
+    public int Speed {  get; private set; }
 
     #endregion
+
+    [SerializeField] private Transform ProjectilePoint;
+
+    //private Enemy _enemy;
+
+    private List<GameObject> _enemyList;
+    private Rigidbody2D _playerRigidbody;
+    private GameObject _enemy;
+
+    
 
     #region Init
 
@@ -22,7 +34,55 @@ public class Player : MonoBehaviour
         AttackSpeed = 0.10f;
         CriticalPercent =  0.00f;
         CriticalDamage = 0;
-        Range = 10;
+        Range = 2;
+        Speed = 100;
+
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+
+        // 임시로 리스트 안에 넣기 
+        _enemyList = new List<GameObject>(GameObject.FindGameObjectsWithTag("Respawn"));
+        //_enemy = _enemyList[0];
+
+    }
+
+    #endregion
+
+    
+
+    private void FixedUpdate()
+    {
+        //적이 범위 밖에 있으면
+        if (Range < Vector2.Distance(transform.position, _enemyList[0].transform.position))
+        {
+            // 평소 달리기 상태로 변경
+            Idle();
+        }
+        else // 범위 안에 있으면 정지 후 공격
+        {
+            _playerRigidbody.velocity = Vector2.zero;
+            Attack(); // TODO : 공격이 Update로 계속 실행되서 projectile이 반복 생성되어 수정 필요함
+        }
+    }
+
+    #region State
+
+    public void Idle()
+    {
+        //_playerRigidbody.velocity = _enemyList[0].transform.position - transform.position;
+        _playerRigidbody.velocity = Vector2.right * Speed * Time.deltaTime;
+    }
+
+
+    public void AttakingRepeat()
+    {
+        InvokeRepeating("Attack", 0, 10);
+    }
+
+    public void Attack()
+    {
+        // 공격 projectile 생성
+        var testProjectile = Manager.Resource.InstantiatePrefab("IceProjectile", ProjectilePoint);
+        testProjectile.transform.Translate(_enemyList[0].transform.position * Time.deltaTime); 
     }
 
     #endregion
