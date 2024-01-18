c9d19e142620887fd08ef12282d21351d2cc7964
dolgang <149345680+dolgang@users.noreply.github.com>
2024-01-18 17:32:15 +0900

    [💡Feature] Summon Prototype Process Add
    - 임시 확률표 추가
    - 반복횟수 만큼 랜덤값을 돌리고 그 값에 맞춰 인덱스를 가져오도록 구성

--- IdleGame/Assets/Scripts/UI/Popup/UIPopupShopSummon.cs
+++ IdleGame/Assets/Scripts/UI/Popup/UIPopupShopSummon.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Linq;
+using Unity.VisualScripting;
 
 public class UIPopupShopSummon : UIPopup
 {
@@ -15,6 +17,12 @@ public class UIPopupShopSummon : UIPopup
     private Button _summonTryBtn_35;
     private Button _closeBtn;
 
+    private int[] probability = new int[] { 5000, 2500, 1500, 700, 250, 50 };
+    private int[] index;
+    private Dictionary<int, int> probabilityTable = new Dictionary<int, int>();
+    private List<int> summonResurt = new List<int>(1000);
+    private int[] testResurt;
+
     #endregion
 
     #region Initialize
@@ -24,6 +32,7 @@ public class UIPopupShopSummon : UIPopup
     {
         base.Init();
         SetButtonEvents();
+        ProbabilityInit();
 
         _player = Manager.Game.Player;
     }
@@ -48,11 +57,65 @@ public class UIPopupShopSummon : UIPopup
         // TODO : 방치 보상 확인 시 보상 획득 추가
         Manager.UI.ClosePopup();
     }
+    #endregion
+
+    #region Summon Test
+
+    private void ProbabilityInit()
+    {
+        int sum = 0;
+        index = new int[probability.Length];
+        testResurt = new int[probability.Length];
+
+        for (int i = 0; i < probability.Length; i++)
+        {
+            index[i] = i;
+        }
+
+        for (int i = 0; i < probability.Length; i++)
+        {
+            sum += probability[i];
+            probabilityTable.Add(sum, index[i]);
+        }
+    }
 
     private void SummonTry(PointerEventData eventData)
     {
-        
+        if (_player.Gems < 0)
+        {
+            Debug.Log("Not enough Gems");
+            return;
+        }
+        Summon();
     }
+
+    private void Summon()
+    {
+        for (int i = 0; i < 35; i++)
+        {
+            summonResurt.Add(Random.Range(0, 10000));
+        }
+        int[] summonResultValue = summonResurt.ToArray();
+        summonResurt.Clear();
+
+        int[] summonValueKey = probabilityTable.Select(x => x.Key).ToArray();
+
+        for (int i = 0; i < summonResultValue.Length; i++)
+        {
+            int getResultKey = summonValueKey.OrderBy(x => (summonResultValue[i] - x > 0)).First(); // 나중에 이진 탐색으로 줄여봅시다
+            probabilityTable.TryGetValue(getResultKey, out int index);
+            summonResurt.Add(index);
+            testResurt[index]++;
+        }
+        Debug.Log($"0 : {testResurt[0]}, 1 : {testResurt[1]}, 2 : {testResurt[2]}, 3 : {testResurt[3]}, 4 : {testResurt[4]}, 5 : {testResurt[5]}");
+        int[] finalResult = summonResurt.ToArray();
+        summonResurt.Clear();
+        for (int i = 0; i < testResurt.Length; i++)
+        {
+            testResurt[i] = 0;
+        }
+    }
+
 
     #endregion
 }
