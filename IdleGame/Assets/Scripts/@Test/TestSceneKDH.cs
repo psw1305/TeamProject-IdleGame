d83395df10371db4eebc13979557fdf36eb93bd5
Ku-dodo <rnehgus9403@gmail.com>
2024-01-18 20:17:01 +0900

    [💡Feature] Reinforce Item
    + item 강화 시스템 추가 및 적용

--- IdleGame/Assets/Scripts/@Test/TestSceneKDH.cs
+++ IdleGame/Assets/Scripts/@Test/TestSceneKDH.cs
@@ -7,6 +7,8 @@ public class TestSceneKDH : BaseScene
     protected override bool Initialize()
     {
         if (!base.Initialize()) return false;
+        // 보스 임시 스폰 포인트 스크립트로 만들기
+        TestBossSpawnPointAdd(out Transform bossSpawnPoint);
 
         Manager.UI.ShowScene<UISceneMain>();
         Manager.Game.SetPosition(playerSpawnPoint.position);
@@ -14,8 +16,18 @@ public class TestSceneKDH : BaseScene
         // 스테이지 전투 구성 & 시작
         Manager.Stage.Initialize();
         Manager.Stage.SetSpawnPoint(enemySpawnPoint);
+        Manager.Stage.SetBossPoint(bossSpawnPoint);
         Manager.Stage.BattleStart();
 
         return true;
+    }
+
+    private void TestBossSpawnPointAdd(out Transform bossSpawnPosition)
+    {
+        var spawnPointTransform = this.transform.Find("_Enemy Spawn Point");
+        var bossSpawnPoint = Instantiate(new GameObject("Boss Spawn Point"), spawnPointTransform.position, Quaternion.identity);
+        bossSpawnPosition = bossSpawnPoint.transform;
+        bossSpawnPoint.transform.position = new Vector2(3.5f, 1.5f);
+        bossSpawnPoint.transform.parent = spawnPointTransform;
     }
 }
