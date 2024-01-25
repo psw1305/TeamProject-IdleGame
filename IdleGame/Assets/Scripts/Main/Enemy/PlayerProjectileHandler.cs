3dc537bf04e9e729b521cfac229e496b33ed54b0
GoHyunkyu <gusrb0296@naver.com>
2024-01-25 16:32:54 +0900

    [🛠️Fix] projectile targeting
    프로젝타일 타게팅이 잘 안되던 것을 수정했습니다.

--- IdleGame/Assets/Scripts/Main/Enemy/PlayerProjectileHandler.cs
+++ IdleGame/Assets/Scripts/Main/Enemy/PlayerProjectileHandler.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 public class PlayerProjectileHandler : ProjectileHandlerBase
 {
     [SerializeField] private float _speed = 1.0f;
-
+    
     public Vector2 TargetPosition;
     private Vector2 direction;
 
-    private void Awake()
+    private void Start()
     {
         direction = (TargetPosition - (Vector2)transform.position);
     }
+
     private void FixedUpdate()
     {
         TrackingTarget();        
     }
+
     protected override void TrackingTarget()
     {
         transform.Translate(direction.normalized * Time.deltaTime * _speed);
