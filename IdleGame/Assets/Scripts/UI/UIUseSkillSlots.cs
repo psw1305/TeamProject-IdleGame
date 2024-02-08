5b4ff0702da0f2bb0c1f832591778c82abf93e0e
Ku-dodo <rnehgus9403@gmail.com>
2024-02-08 20:24:04 +0900

    [➕Add] MainScene UI Skill Slot UI
    + 메인 씬에 스킬 슬롯 UI에 쿨타임 및 지속시간 적용되도록

--- IdleGame/Assets/Scripts/UI/UIUseSkillSlots.cs
+++ IdleGame/Assets/Scripts/UI/UIUseSkillSlots.cs
@@ -4,18 +4,58 @@ using UnityEngine.UI;
 
 public class UIUseSkillSlots : MonoBehaviour
 {
-    [SerializeField] private Image SkillIcon;
-    [SerializeField] private Image Durate;
-    [SerializeField] private Image CoolDown;
+    [SerializeField] private Image ImgSkillIcon;
+    [SerializeField] private Image ImgDurate;
+    [SerializeField] private Image ImgCoolDown;
     private EquipSkillData _equipSkillData;
 
-    public EquipSkillData EquipSkillData => _equipSkillData;
-    public void SetUISkillSlot()
+
+    public void SetUISkillSlot(EquipSkillData equipSkillData)
     {
-        
+        _equipSkillData = equipSkillData;
+        ImgDurate.fillAmount = 0;
+        ImgCoolDown.fillAmount = 0;
+        StopAllCoroutines();
+
+        if (_equipSkillData.SkillID == "Empty")
+        {
+            ImgSkillIcon.gameObject.SetActive(false);
+            return;
+        }
+
+        else
+        {
+            ImgSkillIcon.sprite = Manager.Resource.GetSprite(_equipSkillData.SkillID);
+            ImgSkillIcon.gameObject.SetActive(true);
+            
+            StartCoroutine(SetUIDurateTime());
+        }
     }
-    private void Start()
+
+    public void SetUIUseSkill()
     {
-        
+        StartCoroutine(SetUIDurateTime());
+    }
+
+    IEnumerator SetUIDurateTime()
+    {
+        while (_equipSkillData.SkillScript.CurrentDurateTime != 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            ImgDurate.fillAmount = _equipSkillData.SkillScript.CurrentDurateTime / _equipSkillData.SkillScript.EffectDurateTime;
+        }
+        ImgDurate.fillAmount = 0f;
+
+        StartCoroutine(SetUICoolDown());
+    }
+
+    IEnumerator SetUICoolDown()
+    {
+        while (_equipSkillData.SkillScript.CurrentCoolDown != 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            ImgCoolDown.fillAmount = _equipSkillData.SkillScript.CurrentCoolDown / _equipSkillData.SkillScript.CoolDown;
+        }
+        ImgCoolDown.fillAmount = 0f;
     }
 }
