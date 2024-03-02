b455cbf877a5cf62545a0af3dc497358a5ce3d8d
psw1305 <shshck5@gmail.com>
2024-03-02 20:05:47 +0900

    [🛠️Fix] Login System
    - 로비씬 유니티 파일 복구

--- IdleGame/Assets/Scripts/UI/Scene/UIScene.cs
+++ IdleGame/Assets/Scripts/UI/Scene/UIScene.cs
@@ -22,7 +22,18 @@ public class UIScene : UIBase
             else
             {
                 // 게임종료 팝업 생성
+                var alertPopup = Manager.UI.ShowPopup<UIPopupSystemAlert>();
+                alertPopup.SetData(PopupAlertType.ApplicationQuit, GameQuit);
             }
         }
+    }
+
+    private void GameQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_ANDROID
+        Application.Quit();
+#endif
     }
 }
